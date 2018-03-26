b083966ab6edc8a3e4a71561fe110c196fcf0d35
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-26 23:31:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
-  if array.size == 1
+  case array.size
+  when 1
     return array.join
+  when 2
+    return array.join(" and ")
+  else
+    oxford = array.join(", ")
+    return oxford.insert(oxford.rindex(",")+1," and")
   end
-  oxford = array.join(", ")
-  oxford.insert(oxford.rindex(",")+1," and")
 end
